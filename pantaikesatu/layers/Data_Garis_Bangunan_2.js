697c1fa93816c0e98102273ea373730fd631ff25
Laela Rustiani <lelarustiani@gmail.com>
2025-12-07 00:04:17 +0700

    Delete pantaikesatu directory

--- pantaikesatu/layers/Data_Garis_Bangunan_2.js
+++
@@ -1,8 +0,0 @@
-var json_Data_Garis_Bangunan_2 = {
-"type": "FeatureCollection",
-"name": "Data_Garis_Bangunan_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Lokasi\/Daerah": "Garis Bangunan", "Struktur\/Posisi": "Breakwater, jetty, seadike dan groins. ", "keterangan": "Batu\/beton sepanjang 2,1 km", "Presentase": "63%" }, "geometry": { "type": "LineString", "coordinates": [ [ 95.282299429660853, 5.558347221314853 ], [ 95.295563997751358, 5.569391265090232 ] ] } }
-]
-}
--- pantaikesatu/layers/Data_Pantai_UleeLheue_3.js
+++
@@ -1,8 +0,0 @@
-var json_Data_Pantai_UleeLheue_3 = {
-"type": "FeatureCollection",
-"name": "Data_Pantai_UleeLheue_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Lokasi\/Daerah": "Pantai", "Struktur\/Posisi": "Pantai Ulee Lheue", "keterangan": "Area pelabuhan utama", "Presentase": "-" }, "geometry": { "type": "Point", "coordinates": [ 95.2889, 5.5625 ] } }
-]
-}
