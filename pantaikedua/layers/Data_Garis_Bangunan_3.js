ce750300c65c3a8de40245e4e23c368491a66c99
Laela Rustiani <lelarustiani@gmail.com>
2025-12-07 00:04:04 +0700

    Delete pantaikedua directory

--- pantaikedua/layers/Data_Garis_Bangunan_3.js
+++
@@ -1,8 +0,0 @@
-var json_Data_Garis_Bangunan_3 = {
-"type": "FeatureCollection",
-"name": "Data_Garis_Bangunan_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Lokasi\/Daerah": "Garis Bangunan", "Struktur\/Posisi": "breakwater dan seadike", "keterangan": "Batu\/beton sepanjang 1,26 km", "Presentase": "60%" }, "geometry": { "type": "LineString", "coordinates": [ [ 95.321606204135406, 5.591412835917523 ], [ 95.332165403838701, 5.595529793476578 ] ] } }
-]
-}
